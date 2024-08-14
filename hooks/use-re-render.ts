30b1b753c05b50428fc86dcdbc5e8bb8d2e72147
Anatoliy Aptukov <3_kilos@mail.ru>
2024-08-14 15:53:58 +0300

    1.21.0

--- hooks/use-re-render.ts
+++ hooks/use-re-render.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 function useReRender() {
   const [state, setState] = useState(0);
   return () => {
-    setState((count: number)=> count + 1);
+    setState((count: number) => count + 1);
   };
 };
 export default useReRender;
